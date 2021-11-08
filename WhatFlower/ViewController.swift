227921bd9261ac92cfbea535fd253b2d2f6b9926
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2021-11-08 14:57:29 +0300

    completed project v1

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -8,21 +8,26 @@
 import UIKit
 import CoreML
 import Vision
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
+    let wikipediaURl = "https://en.wikipedia.org/w/api.php"
     let imagePicker = UIImagePickerController()
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         imagePicker.delegate = self
         imagePicker.allowsEditing = true
         imagePicker.sourceType = .camera
-
+        
     }
-
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         
@@ -33,29 +38,73 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             detect(image: convertedCIImage)
             
-            imageView.image = userPickedImage
+            
         }
         imagePicker.dismiss(animated: true, completion: nil)
     }
     
     func detect(image: CIImage) {
         
-        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier(configuration: .init()).model) else {
             fatalError("Cannot import model")
         }
         
         let request = VNCoreMLRequest(model: model) { (request, error) in
-            let classification = request.results?.first as? VNClassificationObservation
+            guard let classification = request.results?.first as? VNClassificationObservation else {
+                fatalError("Could not classify image.")
+            }
             
-            self.navigationItem.title = classification?.identifier.capitalized
+            self.navigationItem.title = classification.identifier.capitalized
+            self.requestInfo(flowerName: classification.identifier)
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
         do {
-         try handler.perform([request])
+            try handler.perform([request])
         }
         catch {
             print(error)
+        }
+    }
+    
+    func requestInfo(flowerName: String) {
+        
+        let parameters : [String:String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop" : "extracts|pageimages",
+            "exintro" : "",
+            "explaintext" : "",
+            "titles" : flowerName,
+            "indexpageids" : "",
+            "redirects" : "1",
+            "pithumbsize" : "500",
+            "indexpageids" : ""
+        ]
+        
+        AF.request(wikipediaURl, method: .get, parameters: parameters).responseJSON { (response) in
+            
+            switch response.result {
+                
+            case .success(let value):
+                
+                let flowerJSON: JSON = JSON(value)
+                
+                let pageid = flowerJSON["query"]["pageids"][0].stringValue
+                
+                let flowerDescription = flowerJSON["query"]["pages"][pageid]["extract"].stringValue
+                
+                let flowerImageURL = flowerJSON["query"]["pages"][pageid]["thumbnail"]["source"].stringValue
+                
+                self.imageView.sd_setImage(with: URL(string: flowerImageURL))
+                
+                self.label.text = flowerDescription
+                
+            case .failure:
+                print("did not get the wikipedia info")
+                
+            }
+            
         }
     }
     
@@ -64,3 +113,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 }
 
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+    return input.rawValue
+}
